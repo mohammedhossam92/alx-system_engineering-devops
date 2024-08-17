e00a803b2287cdf22c972fe9ca52f1ce98bfa3cf
mohammed hossam <mohammedhossam5000@gmail.com>
2024-08-16 20:05:24 -0700

    d

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,13 +10,23 @@ def number_of_subscribers(subreddit):
     Get the number of subscribers for a given subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'user-agent': 'request'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {
+        'User-Agent': '0-sub/1.0 (by /u/SubstantialWar265)'
+    }
 
-    if response.status_code != 200:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code != 200:
+            return 0
+
+        data = response.json().get("data")
+        if not data:
+            return 0
+
+        num_subs = data.get("subscribers", 0)
+        return num_subs
+
+    except requests.RequestException:
+        # In case of network errors or other request exceptions, return 0
         return 0
-
-    data = response.json().get("data")
-    num_subs = data.get("subscribers")
-
-    return num_subs
